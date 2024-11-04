3015dc275b3cc9c977093e61313ccc66a14d9342
AlanDeleon88 <19deleon88@gmail.com>
2024-11-03 18:28:38 -0800

    updated playlist song table to remove index

--- project_src/backend/db/migrations/20220723161218-create-playlist-song.js
+++ project_src/backend/db/migrations/20220723161218-create-playlist-song.js
@@ -54,5 +54,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable(options);
+    await queryInterface.removeIndex(options, ['songId', 'playlistId'], { unique: true });
   }
 };
